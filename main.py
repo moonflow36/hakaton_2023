f0d283a76c65c8cd81e52b1047f18bbd78cdcfa6
moonflow36 <90123042+moonflow36@users.noreply.github.com>
2023-10-01 13:36:58 +0700

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
 from flights_for_our import flights_per_day
-from find_count import count_baggages_per_day
 from nn import fl
+from None_file import day
 from None_file import per_hour
 
 
@@ -12,6 +12,6 @@ from None_file import per_hour
 if '__main__' == '__main__':
     ...
     print(flights_per_day())
-    print(count_baggages_per_day())
     print(fl())
     print(per_hour())
+    print(day())#пассажиры в день
