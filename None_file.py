e9c4ab58b846c2aa45356087d76c21d480a7607e
moonflow36 <90123042+moonflow36@users.noreply.github.com>
2023-10-01 13:40:03 +0700

    None_file.py

--- None_file.py
+++ None_file.py
@@ -1,22 +1,22 @@
 import csv
-with open('bsm_data_train.csv', newline='') as csvfile:
+with open('bsm_data_test.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     times = []
-    passangers = []
     for row in reader:
         times.append(row['MessageReceivedDate'])
     times = sorted(times)
     def per_hour():
         count = 1
+        passangers = []
         per_hour = []
         for i in range(len(times)):
             if i < len(times) - 1:
-                if times[i][11:13] == times[i+1][11:13]:
+                if times[i][:2] == times[i+1][:2]:
                     count += 1
                 else:
                     passangers.append(count)
                     count = 1
-        print(len(passangers)) # пассажиры в час
+
 
         for i in range(24):
             per_hour.append([])
@@ -34,7 +34,16 @@ with open('bsm_data_train.csv', newline='') as csvfile:
         return n
 
 
+    def day():
+        days = []
+        for i in per_hour():
+            days.append(i * 24)
+        return days
+
+
     with open('passangers_hour.csv', 'w') as f:
         writer = csv.writer(f)
         for row in per_hour():
             writer.writerow(str(row))
+
+
