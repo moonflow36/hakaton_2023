6713d3860e7475f4886ac01600f6a5b8a41c6c74
moonflow36 <90123042+moonflow36@users.noreply.github.com>
2023-10-01 13:36:15 +0700

    nn.py

--- nn.py
+++ nn.py
@@ -1,17 +1,20 @@
-import csv
-
-
-with open('flight_rasp_data.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    flights = []
-    x =[]
-    for row in reader:
-        flights.append(row['departure_terminal'])
-    def fl():
-        fl1 = [0, 0]
-        for i in flights:
-            if i == 'B':
-                fl1[0] += 1
-            else:
-                fl1[1] += 1
-        return fl
+import csv                                           
+                                                     
+                                                     
+with open('flight_rasp_data.csv', newline='') as csvf
+    reader = csv.DictReader(csvfile)                 
+    flights = []                                     
+    x =[]                                            
+    for row in reader:                               
+        flights.append(row['departure_terminal'])    
+    def fl():                                        
+        global flights                               
+        flb = []                                     
+        flc =[]                                      
+        for i in flights:                            
+            if i == 'B':                             
+                flb.append(i)                        
+            else:                                    
+                flc.append(i)                        
+        return len(flb), len(flc)                    
+                                                     
